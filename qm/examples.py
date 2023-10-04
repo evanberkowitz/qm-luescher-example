bc6f54b9cdb0f31d710f949e17cd178de677a643
Evan Berkowitz <git@evanberkowitz.com>
2023-10-04 14:56:15 +0200

    Fixes
     - %matlab inline needed for jupyter-jsc
     - No more x[1] in δ-function potential comments
     - Fix example potentials + mystery.

--- qm/examples.py
+++ qm/examples.py
@@ -5,8 +5,8 @@ class harmonic_oscillator:
     def __init__(self, ω=1.):
         self.ω = ω
 
-    def __call__(self, x):
-        return (0.5 * (self.ω * x)**2)
+    def __call__(self, lattice):
+        return (0.5 * (self.ω * lattice.x)**2)
 
 class square_well:
 
@@ -14,8 +14,8 @@ class square_well:
         self.w = width
         self.h = height
 
-    def __call__(self, x):
-        return np.where( np.abs(x) < self.w/2, self.h, 0)
+    def __call__(self, lattice):
+        return np.where( np.abs(lattice.x) < self.w/2, self.h, 0)
 
 mystery_potential = square_well(1, -12)
 
